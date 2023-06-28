6ef86709da42f3223ce6ef458b0e3f7bae8c1b88
Jianing Wei <w1272110852@gmail.com>
2023-06-28 16:03:09 -0400

    modify the homepage, add description,add link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1> Welcome to our Campus and Student Mangement System</h1>
+      <p>The systemis design to manage the data of campus and students</p>
+      <p>The system allow you to view, add, and delete the data</p>
+
+      <Link to="/campuses">Go to Campuses</Link>,
+      <br></br>
+      <Link to="/students">Go to Students</Link>
+    </div>
   )
 }
 
-export default Home
+export default Home;
