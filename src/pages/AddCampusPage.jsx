601b69e30dbfb66abbbbbf0638de5d057769864f
Jianing Wei <w1272110852@gmail.com>
2023-06-29 19:45:56 -0400

    fix error

--- src/pages/AddCampusPage.jsx
+++ src/pages/AddCampusPage.jsx
@@ -10,7 +10,7 @@ const AddCampusPage = () =>{
         dispatch(addCampusThunk(campus));
     };
 
-    return <AddCampus onsubmit={handleSubmit} />
+    return <AddCampus onSubmit={handleSubmit} />
 };
 
 export default AddCampusPage;
