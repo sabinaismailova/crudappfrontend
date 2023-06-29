8b23dd8050544dd2339feab2c65911a5f7c2523f
Jianing Wei <w1272110852@gmail.com>
2023-06-29 14:33:11 -0400

    COMPLETE THE COMPONENT TO EDIT THE CAMPUS INFO

--- src/components/EditCampus.jsx
+++ src/components/EditCampus.jsx
@@ -1,21 +1,63 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { editCampusThunk } from "../redux/campus/campus.action";
 
-const EditCampus=()=>{
-    const campus = useSelector((state) =>state.campuses.singleCampus);
+const EditCampus = () => {
+    const campus = useSelector((state) => state.campuses.singleCampus);
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const {editForm, seteditForm}=useState({});
+    const [editForm, seteditForm] = useState({});
 
-    useEffext(()=>{
+    useEffect(() => {
         console.log('EDIT CAMPUS FIRING IN USE EFFECT');
         seteditForm(campus);
 
 
-    },[campus]);
-    
-   
+    }, [campus]);
+
+    const handleEditSubmit = (event) => {
+        event.preventDefault();
+        console.log('RUNNING DISPATCH FROM EDITCAMPUSTHUNK');
+        dispatch(editCampusThunk(editForm));
+        history.push(`/campuses/${campus.id}`);
+    };
+
+    const HandleInputChange = (event) => {
+        seteditForm({
+            ...editForm,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    return (
+        <form onSubmit={handleEditSubmit}>
+            <label>
+                Name:
+                <input type="text" name="name" value={editForm.name || ""} onChange={HandleInputChange} />
+
+            </label>
+            <label>
+                Address:
+                <input type="text" name="address" value={editForm.address || ""} onChange={HandleInputChange} />
+
+            </label>
+            <label>
+                Image URL:
+                <input type="text" name="imageUrl" value={editForm.imageUrl || ""} onChange={HandleInputChange} />
+            </label>
+            <label>
+                Description:
+                <textarea name="description" value={editForm.description || ""} onChange={HandleInputChange} />
+            </label>
+            <button type="submit">Save</button>
+        </form>
+    );
+
+
+
+
 };
+
+export default EditCampus;
