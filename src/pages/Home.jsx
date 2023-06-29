ceb0147a49286166820502c4eaec9baded599ccd
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-06-29 00:35:54 -0400

    made navbar on home.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './navbar.css'
 
 function Home() {
   return (
     <div>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/campuses">Campuses</Link>
+            </li>
+            <li>
+              <Link to="/students">Students</Link>
+            </li>
+          </ul>
+      </nav>
+
       <h1> Welcome to our Campus and Student Mangement System</h1>
       <p>The systemis design to manage the data of campus and students</p>
       <p>The system allow you to view, add, and delete the data</p>
 
-      <Link to="/campuses">Go to Campuses</Link>,
-      <br></br>
-      <Link to="/students">Go to Students</Link>
+      {
+      // <Link to="/campuses">Go to Campuses</Link>,
+      // <br></br>
+      // <Link to="/students">Go to Students</Link>
+      }
     </div>
   )
 }
