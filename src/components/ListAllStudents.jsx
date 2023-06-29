931dd567ec49c666ef24b392efac62b1cbf9cb91
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-06-28 23:15:29 -0400

    created all students page with dummy data and set up redux reducers and actions for students data

---
+++ src/components/ListAllStudents.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ListAllStudents() {
+  return (
+    <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+        <img src="" alt="student_img" style={{width:"100%"}}/>
+        <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+        </div>
+    </div>
+  )
+}
+
+export default ListAllStudents
--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React from 'react';
+import ListAllStudents from '../components/ListAllStudents';
 
 function Students() {
+  
   return (
-    <div>Students</div>
+    <div>
+      {
+        //div holding header and add student button 
+      }
+      <div>
+        <h1>All Students</h1>
+        <button>Add Student</button>
+      </div>
+      {
+        //div holding all cards with student name and campus
+      }
+      <div style={{display:"inline-flex", flexWrap:"wrap", justifyContent: "center", alignItems: "center"}}>
+        {
+          //divs representing students cards with student info with dummy data 
+        }
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+        <div style={{width:"160px", boxShadow:"0 4px 8px 0 rgba(0,0,0,0.2)", margin:"24px", textAlign: "center"}}>
+          <img src="" alt="student_img" style={{width:"100%"}}/>
+          <div>
+            <h4><b>student name</b></h4>
+            <p>campus name</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
