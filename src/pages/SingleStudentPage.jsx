11513e11cdb0cfde146bf6fb65ec0866a0350699
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-06-29 14:43:34 -0400

    made single student page

---
+++ src/pages/SingleStudentPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SingleStudentPage() {
+  return (
+    <div style={{display:"block", width:"50%", marginLeft:"auto", marginRight:"auto", textAlign:"center"}}>
+        <img src="" alt="student_img" width="240px" height="280px"></img>
+        <div>
+            <h1>Student Name </h1>
+            <h2>Student Campus</h2>
+            <h4>Email: someemail@ttp.com</h4>
+            <h4>GPA: 4.7</h4>
+        </div>
+    </div>
+  )
+}
+
+export default SingleStudentPage
--- src/redux/students/student.action.js
+++ src/redux/students/student.action.js
@@ -2,29 +2,29 @@ import axios from "axios";
 
 import StudentActionType from "./student.types";
 
-export const fetechAllStudents = (payload) =>{
-    console.log("FETECH ALL STUDENTS ACTION");
+export const fetchAllStudents = (payload) =>{
+    console.log("FETCH ALL STUDENTS ACTION");
     return {
-        type: StudentActionType.FETECH_ALL_STUDENTS,
+        type: StudentActionType.FETCH_ALL_STUDENTS,
         payload: payload,
     };
 };
 
-export const fetechSingleStudent = (payload)=>{
-    console.log("FETECH SINGLE STUDENT ACTION");
+export const fetchSingleStudent = (payload)=>{
+    console.log("FETCH SINGLE STUDENT ACTION");
     return {
-        type: StudentActionType.FETECH_SINGLE_STUDENTS,
+        type: StudentActionType.FETCH_SINGLE_STUDENTS,
         payload: payload,
     };
 };
 
-export const fetechAllStudentsThunk = () =>{
+export const fetchAllStudentsThunk = () =>{
     return async (dispatch) =>{
         try{
-            console.log("FETECHALLSTUDENTSTHUNK IS FIRING");
+            console.log("FETCHALLSTUDENTSTHUNK IS FIRING");
             const response = await axios.get('http://localhost:8080/api/getAllStudents');
-            console.log("FETECHALLSTUDENTSTHUNK COMPLETED");
-            dispatch(fetechAllStudents(response.data));
+            console.log("FETCHALLSTUDENTSTHUNK COMPLETED");
+            dispatch(fetchAllStudents(response.data));
         }catch(error){
             console.error(error);
         }
@@ -32,13 +32,13 @@ export const fetechAllStudentsThunk = () =>{
 };
 
 //the api address here is wrong need to fix when backend is ready
-export const fetechSingleStudentThunk = (studentid) =>{
+export const fetchSingleStudentThunk = (studentid) =>{
     return async (dispatch)=>{
         try {
-            console.log("FETECHSINGLESTUDENTSTHUNK IS FIRING");
+            console.log("FETCHSINGLESTUDENTSTHUNK IS FIRING");
             const response = await axios.get(`http://localhost:8080/api/${studentid}`);
-            console.log("FETECHSINGLESTUDENTSTHUNK COMPLETED");
-            dispatch(fetechSingleStudent(response.data));
+            console.log("FETCHSINGLESTUDENTSTHUNK COMPLETED");
+            dispatch(fetchSingleStudent(response.data));
         } catch (error) {
             console.error(error);
         }
